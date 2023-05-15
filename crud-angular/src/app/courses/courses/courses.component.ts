5c2e0696c687f5d517d9c5406cedcad9d7461724
Heytor Mesquita <heytormesquita@gmail.com>
2023-05-15 01:02:58 -0300

    adiciona listagem de itens com dados gerados

--- crud-angular/src/app/courses/courses/courses.component.ts
+++ crud-angular/src/app/courses/courses/courses.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
   templateUrl: './courses.component.html',
   styleUrls: ['./courses.component.scss']
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit {
 
+  courses: Course[] = [
+    { _id: '1', name: 'Angular', category: 'front-end'},
+    { _id: '1', name: 'Java', category: 'back-end'},
+    { _id: '1', name: 'Spring Boot', category: 'back-end'}
+  ];
+  displayedColumns = ['name','category'];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+
+  }
 }
